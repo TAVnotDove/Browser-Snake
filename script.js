5ee9c3d77a1c576538107f8525b88856645ac061
TAVnotDove <slavei.dikovski123@gmail.com>
2022-12-26 22:36:03 +0200

    added border detection

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log("Hello World!")
+
+document.addEventListener("keyup", (e) => {
+    console.log(e.code, "up")
+})
+
+document.addEventListener("keydown", (e) => {
+    console.log(e.code, "down")
+})
+
+const snakeHead = document.querySelector("#snake-head")
+
+const observer = new IntersectionObserver(entries => {
+    if (!entries[0].isIntersecting) {
+        console.log("GAME OVER")
+    }
+}, {
+    root: document.querySelector("#main"),
+    threshold: 1
+})
+
+observer.observe(snakeHead)
