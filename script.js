81d97125ac6d247b861770e56e2bde8d65657fc3
TAVnotDove <slavei.dikovski123@gmail.com>
2022-12-27 16:48:31 +0200

    added basic snake movement

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-console.log("Hello World!")
-
-document.addEventListener("keyup", (e) => {
-    console.log(e.code, "up")
-})
-
-document.addEventListener("keydown", (e) => {
-    console.log(e.code, "down")
-})
-
 const snakeHead = document.querySelector("#snake-head")
+const arrowKeys = ["ArrowRight", "ArrowDown", "ArrowLeft", "ArrowUp"]
+
+const moveInDirection = {
+    up: (element) => {
+        if (element.style.top === "") {
+            element.style.top = "280px" 
+        }
+        element.style.top = element.style.top.split("px")[0]-40+"px"
+    },
+    down: (element) => {
+        if (element.style.top === "") {
+            element.style.top = "280px"
+        }
+        element.style.top = Number(element.style.top.split("px")[0])+40+"px"
+    },
+    left: (element) => {
+        if (element.style.left === "") {
+            element.style.left = "280px"
+        }
+        element.style.left = element.style.left.split("px")[0]-40+"px"
+    },
+    right: (element) => {
+        if (element.style.left === "") {
+            element.style.left = "280px"
+        }
+        element.style.left = Number(element.style.left.split("px")[0]) + 40 +"px"
+    }
+}
 
 const observer = new IntersectionObserver(entries => {
     if (!entries[0].isIntersecting) {
@@ -20,3 +38,21 @@ const observer = new IntersectionObserver(entries => {
 })
 
 observer.observe(snakeHead)
+
+let intervalID = null
+let intervalDelay = 1000
+
+document.addEventListener("keydown", (e) => {
+    if (arrowKeys.includes(e.code)) {
+
+        if (intervalID) {
+            clearInterval(intervalID)
+        }
+
+        moveInDirection[e.code.split("Arrow")[1].toLowerCase()](snakeHead)
+
+        intervalID = setInterval(() => {
+            moveInDirection[e.code.split("Arrow")[1].toLowerCase()](snakeHead)
+        }, intervalDelay);
+    }
+})
